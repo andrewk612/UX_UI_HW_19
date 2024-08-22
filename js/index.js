f4fdd225eb67c1cdaff25df589b06d73016b66db
andrewk612 <160184112+andrewk612@users.noreply.github.com>
2024-08-21 21:31:53 -0500

    code added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 console.log("You're index.js file is loaded correctly!")
+
+function populateChyron() {
+	const snippets = [
+		"My Resume","My Resume","My Resume","My Resume"
+	];
+
+	let snippetContainer = "";
+	for (let i = 0; i < snippets.length; i++) {
+		snippetContainer += `<div class="ticker-item">${snippets[i]}•&nbsp;&nbsp;&nbsp;</div>`;
+	}
+	//write tickers to page
+	const chyron = document.querySelector(".ticker");
+	chyron.innerHTML = snippetContainer;
+
+	//get length in characters of all snippets
+	const snipJoin = snippets.join();
+	characterLength = snipJoin.length;
+
+	// set length of animation in ms to length of all snippet characters
+	// multiplied by multiplier (150)
+	chyron.style.animation = `${characterLength * 300}ms ticker linear infinite`;
+}
+populateChyron();
